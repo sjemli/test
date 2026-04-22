90a1381041a9739d7305a68bcc11911c90188179
sjemli <seifeddinejemli@gmail.com>
2026-04-22 09:18:44 +0200

    Create nnn.sql

---
+++ nnn.sql
@@ -0,0 +1,33 @@
+-- Seed data
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('BOOKING_FIRM',        'booking_firm');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('BRANCH',              'branch');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('CLIENT',              'client');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('ASSET_OWNER',         'asset_owner');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('INVESTMENT_FUND',     'investment_fund');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('ASSET_MANAGER',       'asset_manager');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('FUND_ADMINISTRATOR',  'fund_administrator');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('INVESTMENT_ADVISOR',  'investment_advisor');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('CLEARING_FIRM',       'clearing_firm');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('EXECUTING_FIRM',      'executing_firm');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('FLOOR_BROKER',        'floor_broker');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('EXCHANGE',            'exchange');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('CLEARING_HOUSE',      'clearing_house');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('CUSTODIAN',           'custodian');
+INSERT INTO ref_roles (role_name, description) VALUES
+    ('REGULATORY_AUTHORITY','regulatory_authority');
+
+COMMIT;
